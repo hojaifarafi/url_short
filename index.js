1d346dc876247b9a823e766410ffc2b6f8c23403
hojaifarafi <hojaifaislamrafi@gmail.com>
2025-05-27 01:28:43 +0600

    my 4th commit:

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import path from "path"
 const app = express();
 import { router } from "./routes/router1.js";
+// import {dotenv} from "dotenv"
 //import connectToMongoDB from "./connect.js"
 import mongoose from "mongoose";
 const PORT = process.env.PORT;

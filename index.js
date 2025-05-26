2c368190d76d8d55a6d4b552fef3957fa7405b5b
hojaifarafi <hojaifaislamrafi@gmail.com>
2025-05-27 00:51:44 +0600

    3rd commint

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express"
 import path from "path"
 const app = express();
 import { router } from "./routes/router1.js";
-import connectToMongoDB from "./connect.js"
+//import connectToMongoDB from "./connect.js"
 import mongoose from "mongoose";
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 mongoose.connect("mongodb+srv://poderalo71:86011572@claster0.av8coo8.mongodb.net/?retryWrites=true&w=majority&appName=claster0/test");
   //.then(()=> {console.log("mongodb connected")})
